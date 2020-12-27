1adf8d5a2e19a828feec3de3f8495e39345de767
YUJI <mail@yuji.ne.jp>
2020-12-28 03:13:50 +0900

    Final commit of 2020.

--- manual.sh
+++ manual.sh
@@ -1,7 +1,7 @@
 # Install packages
 # Asunder: Set HTTP Proxy to `freedbtest.dyndns.org` and set port 80.
 sudo pacman -S yay
-yay -S alacritty starship gvim tmux exa bat hexyl docker qemu virt-manager cloudflare-wrangler visual-studio-code-bin quartus-free fcitx-mozc fcitx-im kcm-fcitx dropbox nextdns keepassxc veracrypt nmap balena-etcher makemkv asunder sane minecraft-launcher nerd-fonts-jetbrains-mono nerd-fonts-fira-code
+yay -S alacritty starship gvim tmux exa bat hexyl docker qemu virt-manager cloudflare-wrangler visual-studio-code-bin quartus-free fcitx-mozc fcitx-im kcm-fcitx dropbox nextdns keepassxc veracrypt nmap balena-etcher makemkv asunder sane minecraft-launcher nerd-fonts-jetbrains-mono nerd-fonts-fira-code libz3
 
 # Change default shell
 chsh -s $(which zsh)
